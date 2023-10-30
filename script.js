c43392f12eae58d4dca609af99835956ffe623c8
skoba3678 <skobayankee@yahoo.com>
2023-10-30 13:55:41 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,297 @@
+
+
+
+function Blockonomy()  {
+
+    document.querySelector(".Blockonomy").style.display ="block";
+
+
+}
+
+
+function Bitverse() {
+    document.querySelector(".Bitverse").style.display="block";
+}
+
+
+function Safetron() {
+    document.querySelector(".Safetron").style.display="block";
+
+}
+
+function Quantumia(){
+    document.querySelector(".Quantumia").style.display="block";
+}
+
+function Virturo(){
+    document.querySelector(".Virturo").style.display="block";
+}
+
+
+function Tokenexus() {
+    document.querySelector(".Tokenexus").style.display="block";
+}
+
+function Coinverse() {
+    document.querySelector(".Coinverse").style.display="block";
+}
+
+function Cryptosys() {
+    document.querySelector(".Cryptowave").style.display="block";
+}
+
+function ware() {
+    document.querySelector(".ware").style.display="block";
+}
+
+function core() {
+    document.querySelector(".core").style.display="block";
+}
+
+function alpha() {
+    document.querySelector(".alpha").style.display="block";
+}
+
+function line() {
+    document.querySelector(".line").style.display="block";
+}
+
+function link() {
+    document.querySelector(".link").style.display="block";
+}
+
+function verse() {
+    document.querySelector(".verse").style.display="block";
+}
+
+function plex() {
+    document.querySelector(".plex").style.display="block";
+}
+
+function Quantumium() {
+    document.querySelector(".Quantumium").style.display="block";
+}
+
+function plex() {
+    document.querySelector(".plex").style.display="block";
+}
+
+function Linkraptor() {
+    document.querySelector(".Linkraptor").style.display="block";
+}
+
+function hive() {
+    document.querySelector(".hive").style.display="block";
+}
+
+function Cryptotrix() {
+    document.querySelector(".Cryptotrix").style.display="block";
+}
+
+function connect() {
+    document.querySelector(".connect").style.display="block";
+}
+
+function Digiluxe() {
+    document.querySelector(".Digiluxe").style.display="block";
+}
+
+function sphere() {
+    document.querySelector(".sphere").style.display="block";
+}
+
+function Virtucore() {
+    document.querySelector(".Virtucore").style.display="block";
+}
+
+function Virtupulse() {
+    document.querySelector(".Virtupulse").style.display="block";
+}
+
+function nova() {
+    document.querySelector(".nova").style.display="block";
+}
+
+function Nexocrypt() {
+    document.querySelector(".Nexocrypt").style.display="block";
+}
+
+function byte() {
+    document.querySelector(".byte").style.display="block";
+}
+
+function Cryptoflare() {
+    document.querySelector(".Cryptoflare").style.display="block";
+}
+
+function token() {
+    document.querySelector(".token").style.display="block";
+}
+
+
+function Zikpo() {
+    document.querySelector(".Zikpo").style.display="block";
+}
+
+function Bitgenix() {
+    document.querySelector(".Bitgenix").style.display="block";
+}
+
+function lab() {
+    document.querySelector(".lab").style.display="block";
+}
+
+function Nexocrypt() {
+    document.querySelector(".Nexocrypt").style.display="block";
+}
+function plexus() {
+    document.querySelector(".plexus").style.display="block";
+}
+
+function Aetherion() {
+    document.querySelector(".Aetherion").style.display="block";
+}
+
+function Bitmyst() {
+    document.querySelector(".Bitmyst").style.display="block";
+}
+
+function chain() {
+    document.querySelector(".chain").style.display="block";
+}
+
+function Tokenverse() {
+    document.querySelector(".Tokenverse").style.display="block";
+}
+
+function nexo() {
+    document.querySelector(".nexo").style.display="block";
+}
+
+function Quantron() {
+    document.querySelector(".Quantron").style.display="block";
+}
+
+function Gikmaap() {
+    document.querySelector(".Gikmaap").style.display="block";
+}
+
+function Sloknep() {
+    document.querySelector(".Sloknep").style.display="block";
+}
+
+function Cummest() {
+    document.querySelector(".Cummest").style.display="block";
+}
+
+function Gnubbrag(){
+    document.querySelector(".Gnubbrag").style.display="block";
+}
+
+function Jemwegor() {
+    document.querySelector(".Jemwegor").style.display="block";
+}
+
+function Smebbremust() {
+    document.querySelector(".Smebbremust").style.display="block";
+}
+
+function Slasoddwair() {
+    document.querySelector(".Slasoddwair").style.display="block";
+}
+
+function Goduppog() {
+    document.querySelector(".Goduppog").style.display="block";
+}
+
+function Klalubbness() {
+    document.querySelector(".Klalubbness").style.display="block";
+}
+
+function Raimje() {
+    document.querySelector(".Raimje").style.display="block";
+}
+
+function Snurwum() {
+    document.querySelector(".Snurwum").style.display="block";
+}
+
+function Wimwust() {
+    document.querySelector(".Wimwust").style.display="block";
+}
+
+function Ellbyly() {
+    document.querySelector(".Ellbyly").style.display="block";
+}
+
+function Kniprykall() {
+    document.querySelector(".Kniprykall").style.display="block";
+}
+
+function Phawedlop() {
+    document.querySelector(".Phawedlop").style.display="block";
+}
+
+function Nyneenjall() {
+    document.querySelector(".Nyneenjall").style.display="block";
+
+}
+
+function Flanombil() {
+    document.querySelector(".Flanombil").style.display="block";
+}
+
+function Caikmaam() {
+    document.querySelector(".Caikmaam").style.display="block";
+}
+function Honzbit() {
+    document.querySelector(".Honzbit").style.display="block";
+}
+
+function Fiddwai() {
+    document.querySelector(".Fiddwai").style.display="block";
+}
+
+function Jippit() {
+    document.querySelector(".Jippit").style.display="block";
+}
+
+function Phankkudes() {
+    document.querySelector(".Phankkudes").style.display="block";
+}
+
+function Mimwasell() {
+    document.querySelector(".Mimwasell").style.display="block";
+}
+
+function Fnonukmy() {
+    document.querySelector(".Fnonukmy").style.display="block";
+}
+
+function Cileddes() {
+    document.querySelector(".Cileddes").style.display="block";
+}
+
+function Thodylkaig() {
+    document.querySelector(".Thodylkaig").style.display="block";
+}
+
+function Kneemzac() {
+    document.querySelector(".Kneemzac").style.display="block";
+}
+
+function Klansmog() {
+    document.querySelector(".Klansmog").style.display="block";
+}
+
+function Glumwe() {
+    document.querySelector(".Glumwe").style.display="block";
+}
+
+// time update
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("demo").innerHTML = year;
+
+
